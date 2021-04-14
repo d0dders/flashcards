d9d25958568b9c068aa8d30d020704b3c074f979
d0dders <dodders@outlook.com>
2021-04-14 21:54:24 +0100

    Take users name

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+app.use(express.urlencoded());
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
@@ -9,5 +10,11 @@ app.get('/', (req, res) => {
 app.get('/card', (req, res) => {
     res.render('card', { prompt: "Who is in Grant's tomb?", hint : "Think about who's tomb it is" });
 });
+app.get('/hello', (req, res) => {
+    res.render('hello');
+})
+app.post('/hello', (req, res) => {
+    res.render('hello', { name: req.body.username });
+})
 
 app.listen(3000, () => console.log('Application running on port 3000'));
