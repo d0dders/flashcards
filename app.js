54b444e263150658e3a8d152ff22e11ea325d071
d0dders <dodders@outlook.com>
2021-04-15 16:09:35 +0100

    Redirecting between hello, and index page based on whether username cookie is set

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 app.use(express.urlencoded());
+app.use(cookieParser());
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const name = req.cookies.username;
+    if (name) {
+        res.render('index', { name });
+    } else {
+        res.redirect('/hello');
+    }
+    
 });
 app.get('/card', (req, res) => {
     res.render('card', { prompt: "Who is in Grant's tomb?", hint : "Think about who's tomb it is" });
 });
 app.get('/hello', (req, res) => {
-    res.render('hello');
+    const name = req.cookies.username;
+    if (name) {
+        res.redirect('/');
+    } else {
+        res.render('hello');
+    }
 })
 app.post('/hello', (req, res) => {
-    res.render('hello', { name: req.body.username });
+    res.cookie('username', req.body.username);
+    res.redirect('/');
+})
+app.post('/goodbye', (req, res) => {
+    res.clearCookie('username');
+    res.redirect('/hello');
 })
 
 app.listen(3000, () => console.log('Application running on port 3000'));
