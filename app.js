bc59a21c68d940f16d81bfd44bcd2e6ee8aae639
d0dders <dodders@outlook.com>
2021-04-17 21:12:06 +0100

    Add card data and routes to display

--- app.js
+++ app.js
@@ -6,34 +6,10 @@ app.use(express.urlencoded());
 app.use(cookieParser());
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-    const name = req.cookies.username;
-    if (name) {
-        res.render('index', { name });
-    } else {
-        res.redirect('/hello');
-    }
-    
-});
-app.get('/card', (req, res) => {
-    res.render('card', { prompt: "Who is in Grant's tomb?", hint : "Think about who's tomb it is" });
-});
-app.get('/hello', (req, res) => {
-    const name = req.cookies.username;
-    if (name) {
-        res.redirect('/');
-    } else {
-        res.render('hello');
-    }
-});
-app.post('/hello', (req, res) => {
-    res.cookie('username', req.body.username);
-    res.redirect('/');
-});
-app.post('/goodbye', (req, res) => {
-    res.clearCookie('username');
-    res.redirect('/hello');
-});
+const mainRoutes = require('./routes');
+app.use(mainRoutes);
+const cardRoutes = require('./routes/cards');
+app.use('/cards', cardRoutes);
 
 app.use((req, res, next) => {
     const err = new Error('Not Found');
